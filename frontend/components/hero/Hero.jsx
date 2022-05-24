c1ce69de5a24f8198f0645b334497bba8ebed4eb
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-05-25 01:29:46 +0530

    added landing page

--- frontend/components/hero/Hero.jsx
+++ frontend/components/hero/Hero.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const style = {
     wrapper: `relative backdrop-blur-xl `,
@@ -36,13 +36,13 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className={style.cardContainer}>
-                        <img
+                        <Image
                             className="rounded-t-lg"
                             src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
                             alt="hero"
                         />
                         <div className={style.infoContainer}>
-                            <img
+                            <Image
                                 className="h-[2.25rem] rounded-full"
                                 src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
                                 alt="hero"
